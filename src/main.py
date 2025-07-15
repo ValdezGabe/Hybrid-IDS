90a5eedeceb8cc84cbafbffec6a72655f6b57ef3
ValdezGabe <valdez.gabe@hotmail.com>
2025-07-15 14:42:59 -0700

    Added TrfficAnalyzer class to track connection flows and calculate stats for the packets in real time.

--- src/main.py
+++ src/main.py
@@ -40,3 +40,63 @@ class PacketCapture:
         # Wait for thread to finish executing
         # Terminates cleanly
         self.capture_thread.join()
+
+
+"""
+Name: TrafficAnalyzer
+Description: Analyzes network traffic. Tracks connection flows and calculate statistics for packets in real time
+"""
+class TrafficAnalyzer:
+
+    # Organize data by unique flows using defaultdict()
+    def __init__(self):
+            # Initialize connection attribute to store lists of related packets for each flow
+          self.connections = defaultdict(list)
+            # Initialize flow_stats attribute to store aggregated statistics for each flow:
+            #   - packet count, byte count, start time, time of the most recent packet
+          self.flow_stats = defaultdict(lambda: {
+               'packet_count': 0,
+               'byte_count': 0,
+               'start_time': None,
+               'last_time': None
+          })
+
+    # Process each packet
+    def analyze_packet(self, packet):
+        # If the packet contains IP/TCP layers
+        # Extract the source and destination IPs and ports
+        if IP in packet and TCP in packet:
+              ip_src = packet[IP].src
+              ip_dst = packet[IP].dst
+              port_src = packet[TCP].sport
+              port_dst = packet[TCP].dport
+        
+        # Create flow key from the source and destination IPs and ports
+        # Identifies flow
+        flow_key = (ip_src, ip_dst, port_src, port_dst)
+
+        # Updates the statistics for the flow
+        stats = self.flow_stats[flow_key]
+        stats['packet_count'] += 1
+        stats['byte_count'] += len(packet)
+        curr_time = packet.time
+
+        if not stats['start_time']:
+            stats['start_time'] = curr_time
+        stats['last_time'] = curr_time
+
+        # Calculate and return metrics
+        return self.extract_features(packet, stats)
+    
+
+    # Compute detailed characteristics of the flow and current packet
+    def extract_features(self, packet, stats): 
+        return {
+            'packet_size': len(packet),
+            'flow_duration': stats['last_time'] - stats['start_time'],
+            'packet_rate': stats['packet_count'] / (stats['last_time'] - stats['start_time']),
+            'byte_rate': stats['byte_count'] / (stats['last_time'] - stats['start_time']),
+            'tcp_flags': packet[TCP].flags,
+            'window_size': packet[TCP].window
+        }
+
